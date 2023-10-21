09744de78b4407d5c4b7b88b1281c556a6c937a0
Niyas10 <niyazmuhammad688@gmail.com>
2023-10-21 15:54:08 +0530

    random commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mongoose =  require("mongoose")
 mongoose.connect("mongodb://127.0.0.1:27017/GameGallery")
+
 const express = require("express")
 require('dotenv').config()
 const nocache = require("nocache")
